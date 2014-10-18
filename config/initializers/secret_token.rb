fe8a3c2327187eadc717938f2c3de5e462757d4b
Takahiro Ozawa <t.ozawa.toto@gmail.com>
2014-10-18 12:28:11 +0900

    heroku push用

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '6bd2ce7bfa49ab9bb8742d9c99a4e19464584602a5fb05576146ecd403dc8855700b995f0b0bb5922fd58ea2a5b794efdbea27ec34d15cc082b02190777b4023'
